052883e9c50a5da4dd73970c2396727e52597352
Mohammed Zaheer <mdzaheer78622@gmail.com>
2024-02-01 16:54:45 +0530

    initial

--- app.js
+++ app.js
@@ -3,11 +3,31 @@ let Input = document.querySelector(".input");
 
 function speak(text){
     let Text_Speak = new SpeechSynthesisUtterance(text);
+
     Text_Speak.rate =  1;
     Text_Speak.volume = 1;
     Text_Speak.pitch = 1;
+
     window.speechSynthesis.speak(Text_Speak);
 }
+
+function wishMe(){
+    let Day = new Date();
+    let Hour = Day.getHours();
+
+    if(Hour>=0 && Hour<12){
+        speak("Hello Good Morning");
+    }
+    else if(Hour>12 && Hour<17){
+        speak("Hello Good AfterNoon");
+    }
+    else{
+        speak("Hello Good Evening");
+    }
+}
+
+
 window.addEventListener("load",()=>{
     speak("Initializing ZAK AI....");
+    wishMe();
 });
